9624a7a1151f8bad25916e2946e4e6154f2b2d18
priya <priyankbhalala1911@gmail.com>
2025-03-05 17:15:03 +0530

    add dialoge box

---
+++ src/components/PopupBox.tsx
@@ -0,0 +1,50 @@
+import { Close } from "@mui/icons-material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  Typography,
+} from "@mui/material";
+
+interface PopupBoxProps {
+  open: boolean;
+  onClose: () => void;
+}
+const PopUpBox: React.FC<PopupBoxProps> = ({ open, onClose }) => {
+  return (
+    <Dialog open={open} onClose={onClose} maxWidth="md">
+      <DialogTitle>Want to go back?</DialogTitle>
+      <IconButton
+        aria-label="close"
+        onClick={onClose}
+        sx={(theme) => ({
+          position: "absolute",
+          right: 8,
+          top: 8,
+          color: theme.palette.grey[500],
+        })}
+      >
+        <Close />
+      </IconButton>
+      <DialogContent>
+        <DialogContentText>
+          The seat you previously selected will be cancelled and you will have
+          to re-select.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="outlined" color="primary" onClick={onClose}>
+          Return
+        </Button>
+        <Button variant="contained">
+          <Typography color="warning">Cancelled</Typography>
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+export default PopUpBox;
