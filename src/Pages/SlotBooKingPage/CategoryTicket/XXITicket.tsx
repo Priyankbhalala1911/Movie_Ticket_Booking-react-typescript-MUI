ba1fb8ce3e551dd1b6a2605bca909374550b09ae
priya <priyankbhalala1911@gmail.com>
2025-03-06 15:31:11 +0530

    updated responsive section

--- src/Pages/SlotBooKingPage/CategoryTicket/XXITicket.tsx
+++ src/Pages/SlotBooKingPage/CategoryTicket/XXITicket.tsx
@@ -10,7 +10,13 @@ const XXITicket: React.FC = () => {
           <Avatar sx={{ bgcolor: "#1A2C50", width: "32px", height: "32px" }}>
             <Grade sx={{ color: `${customColors.pastelYellow}` }} />
           </Avatar>
-          <Typography variant="h4" color="primary" flex={1} px="16px">
+          <Typography
+            fontSize={{ lg: "24px", md: "22px", sm: "20px", xs: "18px" }}
+            fontWeight={600}
+            color="primary"
+            flex={1}
+            px="16px"
+          >
             PLAZA INDONESIA XXI
           </Typography>
           <Button
