8b250a74d3716c495f23f4d862211143a48f4328
priya <priyankbhalala1911@gmail.com>
2025-03-07 14:17:10 +0530

    import and export add images

---
+++ src/assets/index.ts
@@ -0,0 +1,84 @@
+import Logo from "./Svgs/Logos/navbarLogo.svg";
+
+import Spiderman from "./Svgs/CarsoualImage/spiderman.svg";
+import YowisBen from "./Svgs/CarsoualImage/Yowis Ben.svg";
+import Ghostbusters from "./Svgs/CarsoualImage/Ghostbusters Afterlife.svg";
+import HouseOfGucci from "./Svgs/CarsoualImage/House of Gucci.svg";
+
+import Advertisemnet1 from "./Svgs/CarsoualAdvertisement/Ads 1.svg";
+import Advertisemnet2 from "./Svgs/CarsoualAdvertisement/Ads 2.svg";
+import Advertisemnet3 from "./Svgs/CarsoualAdvertisement/Ads 3.svg";
+
+import Login from "./Svgs/CreateAccount/login.svg";
+import Registration1 from "./Svgs/CreateAccount/registartion.svg";
+import Registration2 from "./Svgs/CreateAccount/registration2.svg";
+
+import AppleStore from "./Svgs/DownloadImage/App store.svg";
+import GooglePlay from "./Svgs/DownloadImage/Google Play.svg";
+
+import News1 from "./Svgs/MovieNews/News1.svg";
+import News2 from "./Svgs/MovieNews/TIX ID News=Yowis Ben.svg";
+import News3 from "./Svgs/MovieNews/TIX ID News=Ghostbusters.svg";
+import News4 from "./Svgs/MovieNews/TIX ID News=House of Gucci.svg";
+import News5 from "./Svgs/MovieNews/TIX ID News=Raging Fire.svg";
+
+import Poster1 from "./Svgs/MoviePoster/The Matrix Resurrections.svg";
+import Poster2 from "./Svgs/MoviePoster/Resident Evil_ Welcome to Raccoon City.svg";
+import Poster3 from "./Svgs/MoviePoster/Sword Art Online_ Progressive - Aria of a Starless Night.svg";
+import Poster4 from "./Svgs/MoviePoster/Tenet.svg";
+import Poster5 from "./Svgs/MoviePoster/John Wick_ Chapter 3 - Parabellum.svg";
+import Poster6 from "./Svgs/MoviePoster/Avengers_Endgame.svg";
+
+import Dana from "./Svgs/PaymentLogo/Dana Logo.svg";
+import Alfamart from "./Svgs/PaymentLogo/Alfamart Logo.svg";
+import BankBCA from "./Svgs/PaymentLogo/Bank BCA.svg";
+import BankBNI from "./Svgs/PaymentLogo/Bank BNI.svg";
+import BankBRI from "./Svgs/PaymentLogo/Bank BRI.svg";
+import BankMandiri from "./Svgs/PaymentLogo/Bank Mandiri.svg";
+import Indomaret from "./Svgs/PaymentLogo/Indomaret Logo.svg";
+
+import CGV from "./Svgs/Studio/CGV.svg";
+import Cinemapolis from "./Svgs/Studio/cinemapolis.svg";
+import XXI from "./Svgs/Studio/xxi.svg";
+
+import Cinema1 from "./Images/PaymentSuccess/image1.png";
+import Cinema2 from "./Images/PaymentSuccess/image.png";
+
+export {
+  Logo,
+  Spiderman,
+  YowisBen,
+  Ghostbusters,
+  HouseOfGucci,
+  Advertisemnet1,
+  Advertisemnet2,
+  Advertisemnet3,
+  Login,
+  Registration1,
+  Registration2,
+  AppleStore,
+  GooglePlay,
+  News1,
+  News2,
+  News3,
+  News4,
+  News5,
+  Poster1,
+  Poster2,
+  Poster3,
+  Poster4,
+  Poster5,
+  Poster6,
+  Dana,
+  Alfamart,
+  BankBCA,
+  BankBNI,
+  BankBRI,
+  BankMandiri,
+  Indomaret,
+  CGV,
+  Cinemapolis,
+  XXI,
+  Cinema1,
+  Cinema2,
+};
