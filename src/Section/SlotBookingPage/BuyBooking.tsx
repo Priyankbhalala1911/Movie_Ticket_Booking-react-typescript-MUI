c5f78b63d9c65a85198ec14ba44cb66faa7a7e23
priya <priyankbhalala1911@gmail.com>
2025-03-10 15:03:05 +0530

    updated dynamically data in redux store

--- src/Section/SlotBookingPage/BuyBooking.tsx
+++ src/Section/SlotBookingPage/BuyBooking.tsx
@@ -8,57 +8,67 @@ import {
   Typography,
 } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
+import { RootState } from "../../Store";
 
 const BuyBooking: React.FC = () => {
+  const selectedDate = useSelector(
+    (state: RootState) => state.movies.selectedDate
+  );
+  const SelectedSlot = useSelector((state: RootState) => state.shows);
   const navigate = useNavigate();
   return (
-    <Card
-      sx={{
-        my: "30px",
-        border: "1px solid #1A2C50",
-        p: "20px 8px",
-        borderRadius: "12px",
-      }}
-      elevation={0}
-    >
-      <CardContent>
-        <Typography variant="h3" color="primary">
-          GRAND INDONESIA CGV
-        </Typography>
-        <Stack my="24px" gap="12px">
-          <Typography variant="h6" sx={{ color: "#5A637A" }}>
-            Thursday, December 16, 2021
-          </Typography>
-          <Box component="span" display="flex">
-            <Typography flex={1} variant="h4" color="primary">
-              REGULAR 2D
-            </Typography>
-            <Typography variant="h4" color="primary">
-              14.40
-            </Typography>
-          </Box>
-          <Typography sx={{ fontSize: "12px", color: "#9DA8BE" }}>
-            * Seat selection can be done after this
-          </Typography>
-        </Stack>
-      </CardContent>
-      <CardActions>
-        <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          onClick={() => {
-            navigate("/choose-seat");
-            window.scrollTo(0, 0);
+    <>
+      {SelectedSlot.showTimes !== null && (
+        <Card
+          sx={{
+            my: "30px",
+            border: "1px solid #1A2C50",
+            p: "20px 8px",
+            borderRadius: "12px",
           }}
+          elevation={0}
         >
-          <Typography variant="h5" color="warning">
-            BUY NOW
-          </Typography>
-        </Button>
-      </CardActions>
-    </Card>
+          <CardContent>
+            <Typography variant="h3" color="primary">
+              {SelectedSlot.theaterName}
+            </Typography>
+            <Stack my="24px" gap="12px">
+              <Typography variant="h6" sx={{ color: "#5A637A" }}>
+                {selectedDate}
+              </Typography>
+              <Box component="span" display="flex">
+                <Typography flex={1} variant="h4" color="primary">
+                  {SelectedSlot.showType}
+                </Typography>
+                <Typography variant="h4" color="primary">
+                  {SelectedSlot.showTimes}
+                </Typography>
+              </Box>
+              <Typography sx={{ fontSize: "12px", color: "#9DA8BE" }}>
+                * Seat selection can be done after this
+              </Typography>
+            </Stack>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              onClick={() => {
+                navigate("/choose-seat");
+                window.scrollTo(0, 0);
+              }}
+            >
+              <Typography variant="h5" color="warning">
+                BUY NOW
+              </Typography>
+            </Button>
+          </CardActions>
+        </Card>
+      )}
+    </>
   );
 };
 export default BuyBooking;
