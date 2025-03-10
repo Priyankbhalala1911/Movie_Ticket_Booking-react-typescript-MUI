674cb17830d0686495ccb23dea09637cae0cdecf
priya <priyankbhalala1911@gmail.com>
2025-03-10 14:58:48 +0530

    updated main file

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Theme from "./Theme/index.ts";
+import { Provider } from "react-redux";
+import { store } from "./Store/index.ts";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider theme={Theme}>
     <CssBaseline />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ThemeProvider>
 );
