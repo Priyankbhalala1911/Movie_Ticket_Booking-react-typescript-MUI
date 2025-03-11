333a0bb4bc3bdab8b6f67bd6e94f5fc11bfb7ae1
priya <priyankbhalala1911@gmail.com>
2025-03-11 11:20:21 +0530

    search news to updated filter news

--- src/Section/TIXIDNewsPage/FilterNews.tsx
+++ src/Section/TIXIDNewsPage/FilterNews.tsx
@@ -8,9 +8,14 @@ const FilterNews: React.FC = () => {
   const filterNews = useSelector(
     (state: RootState) => state.filterTheater.news
   );
+  const postName = useSelector(
+    (state: RootState) => state.filterTheater.postName
+  );
 
   const FilterData = MovieNewsData.filter(
-    (news) => news.buttonName.toLowerCase() === filterNews.toLowerCase()
+    (news) =>
+      news.buttonName.toLowerCase() === filterNews.toLowerCase() &&
+      news.newsTitle.toLowerCase().includes(postName.toLowerCase())
   );
   const navigate = useNavigate();
   return (
