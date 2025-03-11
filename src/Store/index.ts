30b7f0c28b437bd1de0be69d5eb3537c6d3beba3
priya <priyankbhalala1911@gmail.com>
2025-03-11 09:56:41 +0530

    to add fliter Slice

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import movieSlice from "./Slices/MovieSlice";
 import SeatSlice from "./Slices/SeatSlice";
 import ShowSlice from "./Slices/ShowSlice";
+import FilterSlice from "./Slices/FilterSlice";
 
 export const store = configureStore({
   reducer: {
     movies: movieSlice,
     seats: SeatSlice,
     shows: ShowSlice,
+    filterTheater: FilterSlice,
   },
 });
 
