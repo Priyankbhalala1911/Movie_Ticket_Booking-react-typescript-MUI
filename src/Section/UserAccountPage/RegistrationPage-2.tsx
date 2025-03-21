177b7e1f5f7f61b2d8a7634cc23d7138d467895f
priya <priyankbhalala1911@gmail.com>
2025-03-21 12:09:19 +0530

    To seperate Form in RegistrationForm2

--- src/Section/UserAccountPage/RegistrationPage-2.tsx
+++ src/Section/UserAccountPage/RegistrationPage-2.tsx
@@ -1,37 +1,14 @@
 import { KeyboardBackspace } from "@mui/icons-material";
-import {
-  Box,
-  Button,
-  Card,
-  Container,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useState } from "react";
+import { Box, Button, Container } from "@mui/material";
 import { useNavigate } from "react-router";
 import { Registration2 } from "../../assets";
+import RegistrationForm2 from "../../Forms/RegistrationForm2";
 interface PageProps {
   setDirection: (dir: number) => void;
 }
 
-interface IState {
-  id: number;
-  phoneNumber: string;
-  password: string;
-}
 const RegistrationPage2: React.FC<PageProps> = ({ setDirection }) => {
   const navigate = useNavigate();
-  const [loginUser, setLoginUser] = useState<IState>({
-    id: 0,
-    phoneNumber: "",
-    password: "",
-  });
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = event.target;
-    setLoginUser({ ...loginUser, [name]: value });
-  };
   return (
     <Box
       sx={{
@@ -73,59 +50,7 @@ const RegistrationPage2: React.FC<PageProps> = ({ setDirection }) => {
             Return
           </Button>
         </Box>
-        <Card
-          elevation={3}
-          sx={{
-            maxWidth: "740px",
-            width: "100%",
-            background: "white",
-            justifySelf: "center",
-            p: { lg: "60px", md: "50px", sm: "40px", xs: "30px" },
-          }}
-          component="form"
-        >
-          <Stack gap={"64px"}>
-            <Typography variant="h1" color="primary">
-              TIX ID Register
-            </Typography>
-            <Stack gap="40px">
-              <Stack gap="9px">
-                <Typography
-                  variant="subtitle1"
-                  color="primary"
-                  textTransform="uppercase"
-                >
-                  Email Address
-                </Typography>
-                <TextField
-                  type="text"
-                  placeholder="Enter Email Address"
-                  variant="standard"
-                  name="phoneNumber"
-                  color="primary"
-                  value={loginUser.phoneNumber}
-                  onChange={handleChange}
-                  fullWidth
-                />
-              </Stack>
-            </Stack>
-            <Stack gap="20px">
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{ py: "12px" }}
-                onClick={() => navigate("/")}
-              >
-                <Typography variant="h5" textTransform="capitalize">
-                  Register Now
-                </Typography>
-              </Button>
-              <Typography variant="body1" color="primary" pt={"140px"}>
-                2021 TIX ID - PT.
-              </Typography>
-            </Stack>
-          </Stack>
-        </Card>
+        <RegistrationForm2 />
       </Container>
     </Box>
   );
