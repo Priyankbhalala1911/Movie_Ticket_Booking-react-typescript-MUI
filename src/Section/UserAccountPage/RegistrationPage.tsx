58e52f44694620529de94612f59adf0c64dd8ce3
priya <priyankbhalala1911@gmail.com>
2025-03-21 12:02:18 +0530

    To seperate Form in the RegistrationForm to form section

--- src/Section/UserAccountPage/RegistrationPage.tsx
+++ src/Section/UserAccountPage/RegistrationPage.tsx
@@ -1,43 +1,16 @@
 import { KeyboardBackspace } from "@mui/icons-material";
-import {
-  Box,
-  Button,
-  Card,
-  Container,
-  InputAdornment,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useState } from "react";
+import { Box, Button, Container } from "@mui/material";
 import { useNavigate } from "react-router";
 import { Registration1 } from "../../assets";
+import RegistrationForm1 from "../../Forms/RegistrationForm1";
 
 interface PageProps {
   setDirection: (dir: number) => void;
 }
 
-interface IState {
-  name: string;
-  phoneNumber: string;
-}
 const RegistrationPage: React.FC<PageProps> = ({ setDirection }) => {
   const navigate = useNavigate();
 
-  const [registerUser, setRegisterUser] = useState<IState>({
-    name: "",
-    phoneNumber: "",
-  });
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = event.target;
-    setRegisterUser({ ...registerUser, [name]: value });
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    console.log("registerUser:", registerUser);
-  };
   return (
     <Box
       sx={{
@@ -79,89 +52,7 @@ const RegistrationPage: React.FC<PageProps> = ({ setDirection }) => {
             Return
           </Button>
         </Box>
-        <Card
-          elevation={3}
-          sx={{
-            maxWidth: "740px",
-            width: "100%",
-            background: "white",
-            justifySelf: "center",
-            p: { lg: "60px", md: "50px", sm: "40px", xs: "30px" },
-          }}
-          component="form"
-          onSubmit={handleSubmit}
-        >
-          <Stack gap={"64px"}>
-            <Typography variant="h1" color="primary">
-              TIX ID Register
-            </Typography>
-            <Stack gap="40px">
-              <Stack gap="9px">
-                <Typography
-                  variant="subtitle1"
-                  color="primary"
-                  textTransform="uppercase"
-                >
-                  FUll Name
-                </Typography>
-                <TextField
-                  type="text"
-                  placeholder="Enter Full Name"
-                  variant="standard"
-                  name="name"
-                  color="primary"
-                  value={registerUser.name}
-                  onChange={handleChange}
-                  fullWidth
-                />
-              </Stack>
-
-              <Stack gap="9px">
-                <Typography
-                  variant="subtitle1"
-                  color="primary"
-                  textTransform="uppercase"
-                >
-                  Mobile Number
-                </Typography>
-                <TextField
-                  type="text"
-                  placeholder="Enter Mobile Number"
-                  variant="standard"
-                  name="phoneNumber"
-                  color="primary"
-                  value={registerUser.phoneNumber}
-                  onChange={handleChange}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">+91</InputAdornment>
-                    ),
-                  }}
-                  fullWidth
-                />
-              </Stack>
-            </Stack>
-            <Stack gap="20px">
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{ py: "12px" }}
-                onClick={() => {
-                  navigate("/account/registration-2");
-                  setDirection(1);
-                }}
-                type="submit"
-              >
-                <Typography variant="h5" textTransform="capitalize">
-                  Register Now
-                </Typography>
-              </Button>
-              <Typography variant="body1" color="primary" pt={"100px"}>
-                2021 TIX ID - PT.
-              </Typography>
-            </Stack>
-          </Stack>
-        </Card>
+        <RegistrationForm1 setDirection={setDirection} />
       </Container>
     </Box>
   );
