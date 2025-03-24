ae2523a6262af1223079f0d25b1cc9b5ca05710d
priya <priyankbhalala1911@gmail.com>
2025-03-24 16:01:15 +0530

    Updated authslice to the loginsuccess and logout

--- src/Store/Slices/AuthSlice.ts
+++ src/Store/Slices/AuthSlice.ts
@@ -1,38 +1,24 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 interface AuthData {
-  isAuthenticated: boolean;
-  user: { email: string; password: string } | null;
+  token: string | null;
 }
 
-const storesAuth = localStorage.getItem("authState");
-
-const initialState: AuthData = storesAuth
-  ? JSON.parse(storesAuth)
-  : {
-      isAuthenticated: false,
-      user: null,
-    };
+const initialState: AuthData = { token: localStorage.getItem("token") || null };
 const AuthSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login: (
-      state,
-      action: PayloadAction<{ email: string; password: string }>
-    ) => {
-      const { email, password } = action.payload;
-      if (email === "admin@gmail.com" && password === "Password") {
-        state.isAuthenticated = true;
-        state.user = { email, password };
-
-        localStorage.setItem("authState", JSON.stringify(state));
-      } else {
-        state.isAuthenticated = false;
-      }
+    loginSuccess: (state, action) => {
+      state.token = action.payload;
+      localStorage.setItem("token", action.payload);
+    },
+    logOut: (state) => {
+      state.token = null;
+      localStorage.removeItem("token");
     },
   },
 });
 
-export const { login } = AuthSlice.actions;
+export const { loginSuccess, logOut } = AuthSlice.actions;
 export default AuthSlice.reducer;
