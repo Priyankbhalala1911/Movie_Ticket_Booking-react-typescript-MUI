86df52c9d384349e7d018eb735181be669cf0270
priya <priyankbhalala1911@gmail.com>
2025-03-24 15:59:52 +0530

    Updated authentication with token

--- src/Layout/AuthWrapper.tsx
+++ src/Layout/AuthWrapper.tsx
@@ -1,20 +1,9 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../Store";
-import { Outlet, useLocation, useNavigate } from "react-router";
+import React from "react";
+import { Navigate, Outlet } from "react-router";
 
 const AuthWrapper: React.FC = () => {
-  const isAuthenticated = useSelector(
-    (state: RootState) => state.auth.isAuthenticated
-  );
-  const navigate = useNavigate();
-  const location = useLocation();
+  const token = localStorage.getItem("token");
 
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigate("/account/login", { state: { from: location.pathname } });
-    }
-  }, [isAuthenticated, navigate, location]);
-  return isAuthenticated ? <Outlet /> : null;
+  return token ? <Outlet /> : <Navigate to="/account/login" />;
 };
 export default AuthWrapper;
