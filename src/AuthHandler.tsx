9512fd5c4ddd7e57bc73874f2542262d161e4a0f
priya <priyankbhalala1911@gmail.com>
2025-03-27 18:01:35 +0530

    To chnage checkauthstate

--- src/AuthHandler.tsx
+++ src/AuthHandler.tsx
@@ -1,17 +1,26 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { loginSuccess } from "./Store/Slices/AuthSlice";
+import { loginSuccess, logoutUser } from "./Store/Slices/AuthSlice";
+import axios from "axios";
 
 export const AuthHandler = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const checkAuthState = () => {
-      const userData = localStorage.getItem("user");
-      if (userData) {
-        dispatch(loginSuccess(JSON.parse(userData)));
+    const checkAuthState = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_API_BASE_URL}/me`,
+          { withCredentials: true }
+        );
+
+        dispatch(loginSuccess({ name: response.data }));
+      } catch (err) {
+        dispatch(logoutUser());
+        console.log(err);
       }
     };
+
     checkAuthState();
   }, [dispatch]);
 
