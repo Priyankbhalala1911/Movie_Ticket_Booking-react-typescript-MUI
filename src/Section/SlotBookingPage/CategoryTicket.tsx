7fabbc6211289377d6874d2d5fe5689304df4835
priya <priyankbhalala1911@gmail.com>
2025-03-27 18:05:41 +0530

    To fetch id wise movie on the backend

--- src/Section/SlotBookingPage/CategoryTicket.tsx
+++ src/Section/SlotBookingPage/CategoryTicket.tsx
@@ -7,6 +7,7 @@ import {
   Stack,
   Typography,
   Chip,
+  Skeleton,
 } from "@mui/material";
 import { customColors } from "../../Theme";
 import dayjs from "dayjs";
@@ -17,46 +18,49 @@ import { selectShows } from "../../Store/Slices/ShowSlice";
 const brandColors: { [key: string]: string } = {
   CGV: customColors.CGVColor,
   Cinepolis: customColors.cinepolisBlue,
-  XXI: `linear-gradient(${customColors.xxiGradientStart}, ${customColors.xxiGradientEnd})`,
+  INOX: `linear-gradient(${customColors.xxiGradientStart}, ${customColors.xxiGradientEnd})`,
+  PVR: `linear-gradient(${customColors.pastelYellow},${customColors.CGVColor}, ${customColors.cinepolisBlue})`,
 };
 
-interface Show {
+interface Screens {
   type: string;
   price: string;
-  times: string[];
+  slots: string[];
 }
 
 interface Theater {
   name: string;
   location: string;
-  brand: string;
-  shows: Show[];
+  chain: string;
+  screens: Screens[];
 }
 
 interface TheaterProps {
   theaters: Theater[];
+  loading: boolean;
 }
 
-const CGVTicket: React.FC<TheaterProps> = ({ theaters }) => {
+const CGVTicket: React.FC<TheaterProps> = ({ theaters = [], loading }) => {
   const selectedSlot = useSelector((state: RootState) => state.shows.showTimes);
-  const filterData = useSelector((state: RootState) => state.filterTheater);
+  // const filterData = useSelector((state: RootState) => state.filterTheater);
   const dispatch = useDispatch();
   const now = dayjs();
 
-  const filteredTheaters = theaters.filter(
-    (theater) =>
-      (!filterData.location || theater.location === filterData.location) &&
-      (!filterData.brand || theater.brand === filterData.brand) &&
-      (!filterData.cinema ||
-        theater.name.toLowerCase().includes(filterData.cinema.toLowerCase()))
-  );
+  // const filteredTheaters = theaters.filter(
+  //   (theater) =>
+  //     (!filterData.location || theater.location === filterData.location) &&
+  //     (!filterData.brand || theater.brand === filterData.brand) &&
+  //     (!filterData.cinema ||
+  //       theater.name.toLowerCase().includes(filterData.cinema.toLowerCase()))
+  // );
 
-  console.log(filterData);
-  console.log(filteredTheaters);
+  // console.log(filterData);
+  // console.log(filteredTheaters);
+
   return (
     <Stack gap="24px">
-      {filteredTheaters.length > 0 ? (
-        filteredTheaters.map((theater, index) => {
+      {theaters.length > 0 ? (
+        theaters.map((theater, index) => {
           return (
             <Stack
               key={index}
@@ -70,24 +74,28 @@ const CGVTicket: React.FC<TheaterProps> = ({ theaters }) => {
                   >
                     <Grade sx={{ color: `${customColors.pastelYellow}` }} />
                   </Avatar>
-                  <Typography
-                    color="primary"
-                    flex={1}
-                    px="16px"
-                    fontSize={{
-                      lg: "24px",
-                      md: "22px",
-                      sm: "20px",
-                      xs: "18px",
-                    }}
-                    fontWeight={600}
-                  >
-                    {theater.name}
-                  </Typography>
+                  {loading ? (
+                    <Skeleton />
+                  ) : (
+                    <Typography
+                      color="primary"
+                      flex={1}
+                      px="16px"
+                      fontSize={{
+                        lg: "24px",
+                        md: "22px",
+                        sm: "20px",
+                        xs: "18px",
+                      }}
+                      fontWeight={600}
+                    >
+                      {theater.name}
+                    </Typography>
+                  )}
                   <Chip
-                    label={theater.brand}
+                    label={theater.chain}
                     sx={{
-                      background: brandColors[theater.brand],
+                      background: brandColors[theater.chain],
                       color: "white",
                       borderRadius: "5px",
                       fontWeight: 500,
@@ -100,19 +108,19 @@ const CGVTicket: React.FC<TheaterProps> = ({ theaters }) => {
               </Stack>
 
               <Stack gap="36px">
-                {theater.shows.map((show, index) => (
+                {theater.screens?.map((show, index) => (
                   <Stack key={index} gap="16px">
                     <Box display="flex" alignItems="center">
                       <Typography variant="h4" color="#5A637A" flex={1}>
                         {show.type}
                       </Typography>
                       <Typography variant="subtitle1" color="#5A637A">
-                        {show.price}
+                        {Number(show.price).toFixed(2)}
                       </Typography>
                     </Box>
 
                     <Grid container spacing={2} lg={6}>
-                      {show.times.map((time, idx) => {
+                      {show.slots.map((time, idx) => {
                         const showtime = dayjs()
                           .hour(Number(time.split(":")[0]))
                           .minute(Number(time.split(":")[1]));
