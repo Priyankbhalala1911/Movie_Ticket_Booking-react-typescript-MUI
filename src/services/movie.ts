edf3ea4f17cdb14048bb92ce9e82bf2a726fe616
priya <priyankbhalala1911@gmail.com>
2025-04-01 18:02:06 +0530

    To added movieNews and movieNewsByID fetch data on the backend

--- src/services/movie.ts
+++ src/services/movie.ts
@@ -11,10 +11,17 @@ export const handelMovieApi = async () => {
   }
 };
 
-export const handelMovieById = async (id: string) => {
+export const handelMovieById = async (
+  id: string,
+  city: string,
+  day: number,
+  chain: string
+) => {
   try {
     const response = await axios.get(
-      `${import.meta.env.VITE_BACKEND_API_BASE_URL}/movie/slot-booking/${id}`
+      `${
+        import.meta.env.VITE_BACKEND_API_BASE_URL
+      }/movie/slot-booking/${id}?city=${city}&day=${day}&chain=${chain}`
     );
     return await response.data;
   } catch (error) {
