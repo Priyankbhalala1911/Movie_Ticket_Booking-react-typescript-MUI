bd7526a28a0155d064a7118ea3ee9c1c43eaf89c
priya <priyankbhalala1911@gmail.com>
2025-04-01 18:03:19 +0530

    To add persist on the redux-store

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -4,16 +4,24 @@ import SeatSlice from "./Slices/SeatSlice";
 import ShowSlice from "./Slices/ShowSlice";
 import FilterSlice from "./Slices/FilterSlice";
 import AuthSlice from "./Slices/AuthSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, ShowSlice);
 export const store = configureStore({
   reducer: {
     movies: movieSlice,
     seats: SeatSlice,
-    shows: ShowSlice,
+    shows: persistedReducer,
     filterTheater: FilterSlice,
     auth: AuthSlice,
   },
 });
-
+export const persiste = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
