fb2bc85f6350d55d4d4fee9c26d0435b0e832cf4
priya <priyankbhalala1911@gmail.com>
2025-04-01 18:01:03 +0530

    To Fetch filterNews on the backend

--- src/Section/TIXIDNewsPage/FilterNews.tsx
+++ src/Section/TIXIDNewsPage/FilterNews.tsx
@@ -1,8 +1,16 @@
-import { Box, Button, Card, CardMedia, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardMedia,
+  Skeleton,
+  Typography,
+} from "@mui/material";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { RootState } from "../../Store";
-import { MovieNewsData } from "../../Data/MovieNewsData";
+import { useQuery } from "@tanstack/react-query";
+import { MovieNewsDataApi } from "../../services/movieNews";
 
 const FilterNews: React.FC = () => {
   const filterNews = useSelector(
@@ -12,17 +20,21 @@ const FilterNews: React.FC = () => {
     (state: RootState) => state.filterTheater.postName
   );
 
-  const FilterData = MovieNewsData.filter(
-    (news) =>
-      news.buttonName.toLowerCase() === filterNews.toLowerCase() &&
-      news.newsTitle.toLowerCase().includes(postName.toLowerCase())
-  );
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["movieNews", filterNews, postName],
+    queryFn: () => MovieNewsDataApi(filterNews, postName),
+    staleTime: 0,
+    gcTime: 0,
+  });
+
   const navigate = useNavigate();
+
   return (
     <>
-      {FilterData.map((news, index) => {
-        return (
+      {isLoading &&
+        Array.from(new Array(2)).map((_, index) => (
           <Card
+            key={index}
             elevation={0}
             sx={{
               cursor: "pointer",
@@ -31,7 +43,56 @@ const FilterNews: React.FC = () => {
               alignItems: "center",
               borderRadius: "20px",
               flexDirection: {
-                md: `${index % 2 != 0 ? "row-reverse" : "row"}`,
+                md: `${index % 2 !== 0 ? "row-reverse" : "row"}`,
+                xs: "column",
+              },
+              "&:hover": {
+                background: "rgba(232, 233, 235, 0.61)",
+              },
+            }}
+          >
+            <Skeleton
+              variant="rectangular"
+              width={585}
+              height={410}
+              sx={{ borderRadius: "10px" }}
+            />
+
+            <Box p={{ sm: "8px 25px", xs: "20px 8px" }} maxWidth={"712px"}>
+              <Skeleton
+                variant="rectangular"
+                width={120}
+                height={40}
+                sx={{ mb: "24px" }}
+              />
+
+              <Box display="flex" flexDirection="column" gap="24px">
+                <Skeleton width={650} height={40} />
+                <Skeleton variant="text" width="100%" height={20} />
+                <Skeleton variant="text" width="90%" height={20} />
+                <Skeleton variant="text" width="100%" height={20} />
+              </Box>
+            </Box>
+          </Card>
+        ))}
+      {isError && (
+        <Typography variant="h6" color="error" textAlign="center">
+          {error instanceof Error ? error.message : "Something went wrong"}
+        </Typography>
+      )}
+      {!isLoading && !isError && data?.filterNews?.length > 0 ? (
+        data?.filterNews.map((news: any, index: number) => (
+          <Card
+            key={index}
+            elevation={0}
+            sx={{
+              cursor: "pointer",
+              display: "flex",
+              gap: { lg: "63px", md: "53px", sm: "35px" },
+              alignItems: "center",
+              borderRadius: "20px",
+              flexDirection: {
+                md: `${index % 2 !== 0 ? "row-reverse" : "row"}`,
                 xs: "column",
               },
               "&:hover": {
@@ -42,10 +103,11 @@ const FilterNews: React.FC = () => {
             <CardMedia
               component="img"
               image={news.image}
-              alt={news.newsTitle}
+              alt={news.title}
+              sx={{ borderRadius: "15px" }}
               onClick={() => {
                 window.scrollTo(0, 0);
-                navigate(`/news/video/${news.id}`);
+                navigate(`/news/video/${news.news_id}`);
               }}
             />
 
@@ -56,14 +118,14 @@ const FilterNews: React.FC = () => {
                 sx={{ textTransform: "capitalize", mb: "24px" }}
                 onClick={() => {
                   window.scrollTo(0, 0);
-                  navigate(`/news/${news.id}`);
+                  navigate(`/news/${news.news_id}`);
                 }}
               >
                 {news.buttonName}
               </Button>
               <Box display="flex" flexDirection="column" gap="24px">
                 <Typography variant="h4" color="primary">
-                  {news.newsTitle}
+                  {news.title}
                 </Typography>
                 <Typography
                   color="primary"
@@ -82,8 +144,17 @@ const FilterNews: React.FC = () => {
               </Box>
             </Box>
           </Card>
-        );
-      })}
+        ))
+      ) : (
+        <Typography variant="h5" color="error">
+          {data?.response?.data?.message}
+        </Typography>
+      )}
+      {!isLoading && !isError && data?.filterNews?.length === 0 && (
+        <Typography variant="h6" textAlign="center" color="primary">
+          No News Found
+        </Typography>
+      )}
     </>
   );
 };
