a251935b87dfa079d86f07eeecb88d8df739e1b0
priya <priyankbhalala1911@gmail.com>
2025-04-07 15:08:02 +0530

    To added persistor store

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import App from "./App.tsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Theme from "./Theme/index.ts";
 import { Provider } from "react-redux";
-import { persiste, store } from "./Store/index.ts";
+import { persistor, store } from "./Store/index.ts";
 import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider theme={Theme}>
     <CssBaseline />
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persiste}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
