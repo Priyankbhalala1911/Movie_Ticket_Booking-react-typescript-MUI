3aeeb5fcf70d13b9f9759db98f5dcfc320687939
priya <priyankbhalala1911@gmail.com>
2025-04-07 15:17:58 +0530

    To change a data in the database and added skeleton

--- src/Section/TicketPage/PaymentDetails.tsx
+++ src/Section/TicketPage/PaymentDetails.tsx
@@ -1,6 +1,6 @@
 import { Divider, Stack, Typography } from "@mui/material";
 
-const PaymentDetails: React.FC = () => {
+const PaymentDetails: React.FC<any> = ({ data }) => {
   return (
     <Stack gap="20px" mt="64px">
       <Typography variant="h4" color="primary">
@@ -14,25 +14,11 @@ const PaymentDetails: React.FC = () => {
           mt="12px"
         >
           <Typography variant="body1" color="primary" flex={1}>
-            NO ORDER
-          </Typography>
-          <Typography>
-            Rp. 70.00
-            <b style={{ color: "#414A63" }}> X3</b>
-          </Typography>
-        </Stack>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          mt="12px"
-        >
-          <Typography variant="body1" color="primary" flex={1}>
             REGULAR SEAT
           </Typography>
           <Typography>
-            Rp. 70.00
-            <b style={{ color: "#414A63" }}> X3</b>
+            Rp. {(data?.seat_price ?? 0).toFixed(2)}
+            <b style={{ color: "#414A63" }}> X{data?.seat_number.length}</b>
           </Typography>
         </Stack>
         <Stack
@@ -46,7 +32,7 @@ const PaymentDetails: React.FC = () => {
           </Typography>
           <Typography>
             Rp. 1.00
-            <b style={{ color: "#414A63" }}> X3</b>
+            <b style={{ color: "#414A63" }}> X{data?.seat_number.length}</b>
           </Typography>
         </Stack>
         <Stack
@@ -60,7 +46,7 @@ const PaymentDetails: React.FC = () => {
           </Typography>
           <Typography>
             - Rp. 10.00
-            <b style={{ color: "#414A63" }}> X3</b>
+            <b style={{ color: "#414A63" }}> X{data?.seat_number.length}</b>
           </Typography>
         </Stack>
         <Divider sx={{ mt: "27px" }} />
@@ -74,7 +60,7 @@ const PaymentDetails: React.FC = () => {
             TOTAL PAYMENT
           </Typography>
           <Typography fontWeight={700} color="primary">
-            Rp. 129.00
+            Rp. {(data?.total_amount ?? 0).toFixed(2)}
           </Typography>
         </Stack>
       </Stack>
