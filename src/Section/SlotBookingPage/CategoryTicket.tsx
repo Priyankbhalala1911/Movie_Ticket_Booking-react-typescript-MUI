3c98049d95a084afefe9ca6242b8c8287f821339
priya <priyankbhalala1911@gmail.com>
2025-04-07 15:14:32 +0530

    To change a data with dynamically in the database

--- src/Section/SlotBookingPage/CategoryTicket.tsx
+++ src/Section/SlotBookingPage/CategoryTicket.tsx
@@ -22,187 +22,193 @@ const brandColors: { [key: string]: string } = {
   PVR: `linear-gradient(${customColors.pastelYellow},${customColors.CGVColor}, ${customColors.cinepolisBlue})`,
 };
 
-interface Screens {
-  type: string;
-  price: string;
-  slots: string[];
+interface Showtime {
+  id: string;
+  Time: string;
+  Price: number;
 }
 
-interface Theater {
+interface Screen {
+  id: string;
+  type: string;
+  showtime: Showtime[];
+}
+
+interface Theatre {
+  id: string;
   name: string;
-  location: string;
   chain: string;
-  screens: Screens[];
+  location: string;
+  screens: Screen[];
 }
 
 interface TheaterProps {
-  theaters: Theater[];
+  theatres: Theatre[];
   loading: boolean;
 }
 
-const CGVTicket: React.FC<TheaterProps> = ({ theaters = [], loading }) => {
-  const selectedSlot = useSelector((state: RootState) => state.shows.showTimes);
+const CGVTicket: React.FC<TheaterProps> = ({ theatres = [], loading }) => {
+  const selectedSlot = useSelector((state: RootState) => state.shows.id);
   const selectedDate = useSelector(
     (state: RootState) => state.movies.selectedDate
+  );
+  const selectedMovie = useSelector(
+    (state: RootState) => state.movies.selectedMovie || "Unknown Movie"
   );
   const dispatch = useDispatch();
   const now = dayjs();
 
   return (
     <Stack gap="24px">
-      {(loading ? Array.from(new Array(2)) : theaters).map((theater, index) => {
-        return (
-          <Stack
-            key={index}
-            gap="24px"
-            sx={{ borderBottom: "1px solid #ddd", pb: 3 }}
-          >
-            <Stack gap="18px">
-              <Box display="flex" alignItems="center">
-                <Avatar
-                  sx={{ bgcolor: "#1A2C50", width: "32px", height: "32px" }}
+      {(loading ? Array.from(new Array(2)) : theatres).map((theatre, index) => (
+        <Stack
+          key={theatre?.id || index}
+          gap="24px"
+          sx={{ borderBottom: "1px solid #ddd", pb: 3 }}
+        >
+          <Stack gap="18px">
+            <Box display="flex" alignItems="center">
+              <Avatar
+                sx={{ bgcolor: "#1A2C50", width: "32px", height: "32px" }}
+              >
+                <Grade sx={{ color: `${customColors.pastelYellow}` }} />
+              </Avatar>
+              {loading ? (
+                <Skeleton
+                  width="20%"
+                  height={30}
+                  variant="text"
+                  animation="wave"
+                  sx={{ flex: 1 }}
+                />
+              ) : (
+                <Typography
+                  color="primary"
+                  flex={1}
+                  px="16px"
+                  fontSize={{
+                    lg: "24px",
+                    md: "22px",
+                    sm: "20px",
+                    xs: "18px",
+                  }}
+                  fontWeight={600}
                 >
-                  <Grade sx={{ color: `${customColors.pastelYellow}` }} />
-                </Avatar>
-                {loading ? (
-                  <Skeleton
-                    width="20%"
-                    height={30}
-                    variant="text"
-                    animation="wave"
-                    sx={{ flex: 1 }}
-                  />
-                ) : (
-                  <Typography
-                    color="primary"
-                    flex={1}
-                    px="16px"
-                    fontSize={{
-                      lg: "24px",
-                      md: "22px",
-                      sm: "20px",
-                      xs: "18px",
-                    }}
-                    fontWeight={600}
-                  >
-                    {theater.name}
-                  </Typography>
-                )}
-                {loading ? (
-                  <Skeleton width={60} height={30} variant="rectangular" />
-                ) : (
-                  <Chip
-                    label={theater?.chain}
-                    sx={{
-                      background: brandColors[theater.chain],
-                      color: "white",
-                      borderRadius: "5px",
-                      fontWeight: 500,
-                    }}
-                  />
-                )}
-              </Box>
-              <Typography variant="body1" color="#5A637A">
-                {theater?.location}
-              </Typography>
-            </Stack>
+                  {theatre.name}
+                </Typography>
+              )}
+              {loading ? (
+                <Skeleton width={60} height={30} variant="rectangular" />
+              ) : (
+                <Chip
+                  label={theatre?.chain}
+                  sx={{
+                    background: brandColors[theatre.chain],
+                    color: "white",
+                    borderRadius: "5px",
+                    fontWeight: 500,
+                  }}
+                />
+              )}
+            </Box>
+            <Typography variant="body1" color="#5A637A">
+              {theatre?.location}
+            </Typography>
+          </Stack>
 
-            <Stack gap="36px">
-              {(loading ? Array.from(new Array(2)) : theater.screens)?.map(
-                (show: any, index: number) => (
-                  <Stack key={index} gap="16px">
-                    <Box display="flex" alignItems="center">
-                      {loading ? (
-                        <Skeleton width="40%" height={31} sx={{ flex: 1 }} />
-                      ) : (
-                        <Typography variant="h4" color="#5A637A" flex={1}>
-                          {show.type}
-                        </Typography>
-                      )}
-                      {loading ? (
-                        <Skeleton width="20%" height={31} />
-                      ) : (
-                        <Typography variant="subtitle1" color="#5A637A">
-                          {Number(show.price).toFixed(2)}
-                        </Typography>
-                      )}
-                    </Box>
+          <Stack gap="36px">
+            {(loading ? Array.from(new Array(2)) : theatre.screens)?.map(
+              (screen: Screen, index: number) => (
+                <Stack key={index} gap="16px">
+                  <Box display="flex" alignItems="center">
+                    {loading ? (
+                      <Skeleton width="40%" height={31} sx={{ flex: 1 }} />
+                    ) : (
+                      <Typography variant="h4" color="#5A637A" flex={1}>
+                        {screen.type}
+                      </Typography>
+                    )}
+                  </Box>
 
-                    <Grid container spacing={2} lg={6}>
-                      {(loading ? Array.from(new Array(4)) : show.slots).map(
-                        (time: any, idx: number) => {
-                          if (loading) {
-                            return (
-                              <Grid item lg={3} key={idx}>
-                                <Skeleton width="100%" height={36} />
-                              </Grid>
-                            );
-                          }
-                          const showtime = dayjs()
-                            .hour(Number(time.split(":")[0]))
-                            .minute(Number(time.split(":")[1]));
-
-                          const isPastTime = showtime.isBefore(now);
-                          const isDisabled = isPastTime;
-
+                  <Grid container spacing={2} lg={6}>
+                    {(loading ? Array.from(new Array(4)) : screen.showtime).map(
+                      (show: Showtime, idx: number) => {
+                        if (loading) {
                           return (
                             <Grid item lg={3} key={idx}>
-                              <Button
-                                variant={
-                                  selectedSlot === time ? "contained" : "text"
-                                }
-                                color="primary"
-                                onClick={() => {
-                                  if (!isDisabled) {
-                                    dispatch(
-                                      selectShows({
-                                        time,
-                                        type: show.type,
-                                        price: show.price,
-                                        theaterName: theater.name,
-                                        location: theater.location,
-                                        date: selectedDate,
-                                      })
-                                    );
-                                  }
-                                }}
-                                sx={{
-                                  border:
-                                    selectedSlot === time
-                                      ? "none"
-                                      : "1px solid #9DA8BE",
-                                  bgcolor:
-                                    selectedSlot === time
-                                      ? "primary.main"
-                                      : "transparent",
-                                  color:
-                                    selectedSlot === time ? "white" : "black",
-                                  "&:hover": {
-                                    bgcolor: "#282764",
-                                    color: "white",
-                                  },
-                                  "&:disabled": {
-                                    color: "#9DA8BE",
-                                    border: "1px solid #ddd",
-                                    bgcolor: "#DADFE8",
-                                  },
-                                }}
-                                disabled={isDisabled}
-                              >
-                                {time}
-                              </Button>
+                              <Skeleton width="100%" height={36} />
                             </Grid>
                           );
                         }
-                      )}
-                    </Grid>
-                  </Stack>
-                )
-              )}
-            </Stack>
+
+                        const showtime = dayjs()
+                          .hour(Number(show.Time.split(":")[0]))
+                          .minute(Number(show.Time.split(":")[1]));
+
+                        const isPastTime = showtime.isBefore(now);
+                        const isDisabled = isPastTime;
+
+                        return (
+                          <Grid item lg={3} key={show.id}>
+                            <Button
+                              variant={
+                                selectedSlot === show.Time
+                                  ? "contained"
+                                  : "text"
+                              }
+                              color="primary"
+                              onClick={() => {
+                                if (!isDisabled) {
+                                  dispatch(
+                                    selectShows({
+                                      id: show.id,
+                                      time: show.Time,
+                                      type: screen.type,
+                                      price: show.Price,
+                                      theaterName: theatre.name,
+                                      location: theatre.location,
+                                      date: selectedDate,
+                                      movie_title: selectedMovie,
+                                    })
+                                  );
+                                }
+                              }}
+                              sx={{
+                                border:
+                                  selectedSlot === show.id
+                                    ? "none"
+                                    : "1px solid #9DA8BE",
+                                bgcolor:
+                                  selectedSlot === show.id
+                                    ? "primary.main"
+                                    : "transparent",
+                                color:
+                                  selectedSlot === show.id ? "white" : "black",
+                                "&:hover": {
+                                  bgcolor: "#282764",
+                                  color: "white",
+                                },
+                                "&:disabled": {
+                                  color: "#9DA8BE",
+                                  border: "1px solid #ddd",
+                                  bgcolor: "#DADFE8",
+                                },
+                              }}
+                              disabled={isDisabled}
+                            >
+                              {show.Time}
+                            </Button>
+                          </Grid>
+                        );
+                      }
+                    )}
+                  </Grid>
+                </Stack>
+              )
+            )}
           </Stack>
-        );
-      })}{" "}
+        </Stack>
+      ))}
     </Stack>
   );
 };
