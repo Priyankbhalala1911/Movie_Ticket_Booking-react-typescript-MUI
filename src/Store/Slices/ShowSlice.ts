7d542fafeea382519350d8f5fec5a6f1335b33be
priya <priyankbhalala1911@gmail.com>
2025-04-07 15:30:17 +0530

    To updated show slice

--- src/Store/Slices/ShowSlice.ts
+++ src/Store/Slices/ShowSlice.ts
@@ -2,21 +2,25 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "..";
 
 interface showState {
+  id: string | null;
   showTimes: string | null;
-  showPrice: string | null;
+  showPrice: number | null;
   showType: string | null;
   theaterName: string | null;
   location: string | null;
   date: string | null;
+  movie_title: string | null;
 }
 
 const initialState: showState = {
+  id: null,
   showTimes: null,
   showPrice: null,
   showType: null,
   theaterName: null,
   location: null,
   date: null,
+  movie_title: null,
 };
 
 const ShowSlice = createSlice({
@@ -26,25 +30,42 @@ const ShowSlice = createSlice({
     selectShows: (
       state,
       action: PayloadAction<{
+        id: string;
         type: string;
-        price: string;
+        price: number;
         time: string;
         theaterName: string;
         location: string;
         date: string;
+        movie_title: string;
       }>
     ) => {
+      state.id = action.payload.id;
       state.showTimes = action.payload.time;
       state.showPrice = action.payload.price;
       state.showType = action.payload.type;
       state.theaterName = action.payload.theaterName;
       state.location = action.payload.location;
       state.date = action.payload.date;
+      state.movie_title = action.payload.movie_title;
+    },
+    selectTime: (state, action: PayloadAction<string>) => {
+      state.showTimes = action.payload;
+    },
+    clearSlot: (state) => {
+      state.id = null;
+      state.showTimes = null;
+      state.showPrice = null;
+      state.showType = null;
+      state.theaterName = null;
+      state.location = null;
+      state.date = null;
+      state.movie_title = null;
     },
   },
 });
 
-export const { selectShows } = ShowSlice.actions;
+export const { selectShows, clearSlot, selectTime } = ShowSlice.actions;
 export const selectedShow = (state: RootState) => state.shows;
 
 export default ShowSlice.reducer;
