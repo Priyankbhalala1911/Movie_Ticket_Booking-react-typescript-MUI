e04516fd12cda27183a6dcbe8a94fe61cd27a970
priya <priyankbhalala1911@gmail.com>
2025-04-15 17:09:00 +0530

    To Added error handling message

--- src/Section/MyTicketPage/TrasactionList.tsx
+++ src/Section/MyTicketPage/TrasactionList.tsx
@@ -19,7 +19,7 @@ const ActiveTicket: React.FC = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["myticket"],
     queryFn: () => MyTicket(),
     staleTime: 0,
@@ -38,143 +38,149 @@ const ActiveTicket: React.FC = () => {
       flex={1}
     >
       <Typography variant={isMobile ? "h4" : "h2"} color="primary">
-        My Ticket
+        My Transaction
       </Typography>
       <Typography variant="body1" color="primary">
         List of tickets and transactions you have made
       </Typography>
 
       <Stack width="100%" spacing={2}>
-        {isLoading
-          ? Array.from({ length: skeletonCount }).map((_, index) => (
-              <Box key={index}>
-                <Card
-                  elevation={0}
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    gap: { lg: "48px", md: "36px", sm: "15px", xs: "5px" },
-                    py: "16px",
-                  }}
-                >
-                  <Skeleton
-                    variant="rectangular"
-                    width={isMobile ? 85 : 135}
-                    height={isMobile ? 100 : 202}
-                    sx={{ borderRadius: "10px" }}
-                  />
-                  <CardContent sx={{ width: "100%" }}>
-                    <Stack gap="12px">
-                      <Skeleton
-                        variant="text"
-                        width="60%"
-                        height={isMobile ? 20 : 30}
-                      />
-                      <Skeleton
-                        variant="text"
-                        width="40%"
-                        height={isMobile ? 15 : 20}
-                      />
-                      <Skeleton
-                        variant="text"
-                        width="80%"
-                        height={isMobile ? 15 : 20}
-                      />
-                    </Stack>
-                  </CardContent>
-                  <CardActions>
+        {isLoading ? (
+          Array.from({ length: skeletonCount }).map((_, index) => (
+            <Box key={index}>
+              <Card
+                elevation={0}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: { lg: "48px", md: "36px", sm: "15px", xs: "5px" },
+                  py: "16px",
+                }}
+              >
+                <Skeleton
+                  variant="rectangular"
+                  width={isMobile ? 85 : 135}
+                  height={isMobile ? 100 : 202}
+                  sx={{ borderRadius: "10px" }}
+                />
+                <CardContent sx={{ width: "100%" }}>
+                  <Stack gap="12px">
                     <Skeleton
-                      variant="rounded"
-                      width={isMobile ? 60 : 100}
-                      height={isMobile ? 20 : 35}
+                      variant="text"
+                      width="60%"
+                      height={isMobile ? 20 : 30}
                     />
-                  </CardActions>
-                </Card>
-                {index !== skeletonCount - 1 && <Divider />}
-              </Box>
-            ))
-          : data?.map((ticket: any, index: number) => (
-              <Box key={index}>
-                <Card
-                  elevation={0}
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    gap: { lg: "48px", md: "36px", sm: "15px", xs: "5px" },
-                    py: "16px",
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    image={ticket.movie_poster}
-                    alt="movie"
-                    sx={{
-                      width: { xs: "85px", sm: "135px" },
-                      height: { xs: "auto", sm: "202px" },
-                      borderRadius: "10px",
-                    }}
+                    <Skeleton
+                      variant="text"
+                      width="40%"
+                      height={isMobile ? 15 : 20}
+                    />
+                    <Skeleton
+                      variant="text"
+                      width="80%"
+                      height={isMobile ? 15 : 20}
+                    />
+                  </Stack>
+                </CardContent>
+                <CardActions>
+                  <Skeleton
+                    variant="rounded"
+                    width={isMobile ? 60 : 100}
+                    height={isMobile ? 20 : 35}
                   />
-                  <CardContent sx={{ width: "100%" }}>
-                    <Stack gap="12px">
-                      <Typography
-                        variant={isMobile ? "body2" : "h5"}
-                        color="primary"
-                      >
-                        {ticket.movie_title}
-                      </Typography>
-                      <Typography
-                        variant={isMobile ? "body2" : "body1"}
-                        color="primary"
-                      >
-                        {ticket.show_date}, {ticket.show_time}
-                      </Typography>
-                      <Box
-                        display="flex"
-                        alignItems="center"
-                        gap={1}
-                        flexWrap="wrap"
-                      >
-                        <Place sx={{ color: "#9DA8BE" }} />
-                        <Typography
-                          color="#9DA8BE"
-                          variant={isMobile ? "caption" : "body2"}
-                        >
-                          {ticket.location}
-                        </Typography>
-                        <Typography
-                          color="primary"
-                          variant={isMobile ? "caption" : "body2"}
-                          fontWeight={500}
-                        >
-                          ({ticket.show_type})
-                        </Typography>
-                      </Box>
-                    </Stack>
-                  </CardContent>
-                  <CardActions>
-                    <Box
-                      bgcolor={
-                        ticket.payment_status ? "success.main" : "error.main"
-                      }
-                      textAlign="center"
-                      color="white"
-                      p={{
-                        lg: "10px 24px",
-                        md: "8px 20px",
-                        sm: "6px 16px",
-                        xs: "4px 12px",
-                      }}
-                      borderRadius="4px"
-                      sx={{ fontSize: { md: "16px", xs: "8px" } }}
-                      fontWeight={500}
+                </CardActions>
+              </Card>
+              {index !== skeletonCount - 1 && <Divider />}
+            </Box>
+          ))
+        ) : !isError ? (
+          data?.map((ticket: any, index: number) => (
+            <Box key={index}>
+              <Card
+                elevation={0}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: { lg: "48px", md: "36px", sm: "15px", xs: "5px" },
+                  py: "16px",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  image={ticket.movie_poster}
+                  alt="movie"
+                  sx={{
+                    width: { xs: "85px", sm: "135px" },
+                    height: { xs: "auto", sm: "202px" },
+                    borderRadius: "10px",
+                  }}
+                />
+                <CardContent sx={{ width: "100%" }}>
+                  <Stack gap="12px">
+                    <Typography
+                      variant={isMobile ? "body2" : "h5"}
+                      color="primary"
                     >
-                      {ticket.payment_status ? "Succeeded" : "Failed"}
+                      {ticket.movie_title}
+                    </Typography>
+                    <Typography
+                      variant={isMobile ? "body2" : "body1"}
+                      color="primary"
+                    >
+                      {ticket.show_date}, {ticket.show_time}
+                    </Typography>
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      gap={1}
+                      flexWrap="wrap"
+                    >
+                      <Place sx={{ color: "#9DA8BE" }} />
+                      <Typography
+                        color="#9DA8BE"
+                        variant={isMobile ? "caption" : "body2"}
+                      >
+                        {ticket.location}
+                      </Typography>
+                      <Typography
+                        color="primary"
+                        variant={isMobile ? "caption" : "body2"}
+                        fontWeight={500}
+                      >
+                        ({ticket.show_type})
+                      </Typography>
                     </Box>
-                  </CardActions>
-                </Card>
-                {index !== data.length - 1 && <Divider />}
-              </Box>
-            ))}
+                  </Stack>
+                </CardContent>
+                <CardActions>
+                  <Box
+                    bgcolor={
+                      ticket.payment_status ? "success.main" : "error.main"
+                    }
+                    textAlign="center"
+                    color="white"
+                    p={{
+                      lg: "10px 24px",
+                      md: "8px 20px",
+                      sm: "6px 16px",
+                      xs: "4px 12px",
+                    }}
+                    borderRadius="4px"
+                    sx={{ fontSize: { md: "16px", xs: "8px" } }}
+                    fontWeight={500}
+                  >
+                    {ticket.payment_status ? "Succeeded" : "Failed"}
+                  </Box>
+                </CardActions>
+              </Card>
+              {index !== data.length - 1 && <Divider />}
+            </Box>
+          ))
+        ) : (
+          <Typography variant="h5" color="error">
+            {data?.response?.data?.message}
+          </Typography>
+        )}
       </Stack>
     </Stack>
   );
