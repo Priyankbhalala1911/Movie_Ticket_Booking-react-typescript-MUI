b9be32c25d53346aef098f84698808b2ee2273a9
priya <priyankbhalala1911@gmail.com>
2025-04-15 17:08:33 +0530

    To added error handling message

--- src/Section/MyTicketPage/ActiveTicket.tsx
+++ src/Section/MyTicketPage/ActiveTicket.tsx
@@ -20,13 +20,12 @@ const ActiveTicket: React.FC = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const navigate = useNavigate();
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ["myticket"],
     queryFn: () => MyTicket(),
     staleTime: 0,
     gcTime: 0,
   });
-
   const skeletonArray = Array(2).fill(0);
 
   return (
@@ -46,101 +45,107 @@ const ActiveTicket: React.FC = () => {
       </Typography>
 
       <Stack width="100%">
-        {isLoading
-          ? skeletonArray.map((_, index) => (
-              <Box key={index}>
-                <Card
-                  elevation={0}
+        {isLoading ? (
+          skeletonArray.map((_, index) => (
+            <Box key={index}>
+              <Card
+                elevation={0}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: { lg: "48px", md: "36px", sm: "15px", xs: "5px" },
+                  my: "16px",
+                }}
+              >
+                <Skeleton
+                  variant="rectangular"
+                  width={135}
+                  height={202}
+                  sx={{ borderRadius: "10px" }}
+                />
+                <CardContent sx={{ width: "100%" }}>
+                  <Stack gap="12px">
+                    <Skeleton variant="text" width="50%" height={30} />
+                    <Skeleton variant="text" width="30%" height={20} />
+                    <Skeleton variant="text" width="80%" height={20} />
+                  </Stack>
+                </CardContent>
+              </Card>
+              {index !== skeletonArray.length - 1 && <Divider />}
+            </Box>
+          ))
+        ) : data?.length > 0 && !isError ? (
+          data?.map((ticket: any, index: number) => (
+            <Box key={index}>
+              <Card
+                elevation={0}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: { lg: "48px", md: "36px", sm: "15px", xs: "5px" },
+                  my: "16px",
+                  cursor: "pointer",
+                  "&:hover": { bgcolor: "rgba(0, 0, 0, 0.1)" },
+                }}
+                onClick={() => navigate(`/ticket/${ticket.id}`)}
+              >
+                <CardMedia
+                  component="img"
+                  image={ticket.movie_poster}
+                  alt="movie"
                   sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    gap: { lg: "48px", md: "36px", sm: "15px", xs: "5px" },
-                    my: "16px",
+                    width: "135px",
+                    height: "202px",
+                    borderRadius: "10px",
                   }}
-                >
-                  <Skeleton
-                    variant="rectangular"
-                    width={135}
-                    height={202}
-                    sx={{ borderRadius: "10px" }}
-                  />
-                  <CardContent sx={{ width: "100%" }}>
-                    <Stack gap="12px">
-                      <Skeleton variant="text" width="50%" height={30} />
-                      <Skeleton variant="text" width="30%" height={20} />
-                      <Skeleton variant="text" width="80%" height={20} />
-                    </Stack>
-                  </CardContent>
-                </Card>
-                {index !== skeletonArray.length - 1 && <Divider />}
-              </Box>
-            ))
-          : data?.map((ticket: any, index: number) => (
-              <Box key={index}>
-                <Card
-                  elevation={0}
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    gap: { lg: "48px", md: "36px", sm: "15px", xs: "5px" },
-                    my: "16px",
-                    cursor: "pointer",
-                    "&:hover": { bgcolor: "rgba(0, 0, 0, 0.1)" },
-                  }}
-                  onClick={() => navigate(`/ticket/${ticket.id}`)}
-                >
-                  <CardMedia
-                    component="img"
-                    image={ticket.movie_poster}
-                    alt="movie"
-                    sx={{
-                      width: "135px",
-                      height: "202px",
-                      borderRadius: "10px",
-                    }}
-                  />
+                />
 
-                  <CardContent sx={{ width: "100%" }}>
-                    <Stack gap="12px">
+                <CardContent sx={{ width: "100%" }}>
+                  <Stack gap="12px">
+                    <Typography
+                      variant={isMobile ? "body2" : "h5"}
+                      color="primary"
+                    >
+                      {ticket.movie_title}
+                    </Typography>
+                    <Typography
+                      variant={isMobile ? "body2" : "body1"}
+                      color="primary"
+                    >
+                      {ticket.show_date}, {ticket.show_time}
+                    </Typography>
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      gap={1}
+                      flexWrap="wrap"
+                    >
+                      <Place sx={{ color: "#9DA8BE" }} />
                       <Typography
-                        variant={isMobile ? "body2" : "h5"}
-                        color="primary"
+                        color="#9DA8BE"
+                        variant={isMobile ? "caption" : "body2"}
                       >
-                        {ticket.movie_title}
+                        {ticket.location}
                       </Typography>
                       <Typography
-                        variant={isMobile ? "body2" : "body1"}
                         color="primary"
+                        variant={isMobile ? "caption" : "body2"}
+                        fontWeight={500}
                       >
-                        {ticket.show_date}, {ticket.show_time}
+                        ({ticket.show_type})
                       </Typography>
-                      <Box
-                        display="flex"
-                        alignItems="center"
-                        gap={1}
-                        flexWrap="wrap"
-                      >
-                        <Place sx={{ color: "#9DA8BE" }} />
-                        <Typography
-                          color="#9DA8BE"
-                          variant={isMobile ? "caption" : "body2"}
-                        >
-                          {ticket.location}
-                        </Typography>
-                        <Typography
-                          color="primary"
-                          variant={isMobile ? "caption" : "body2"}
-                          fontWeight={500}
-                        >
-                          ({ticket.show_type})
-                        </Typography>
-                      </Box>
-                    </Stack>
-                  </CardContent>
-                </Card>
-                {index !== data.length - 1 && <Divider />}
-              </Box>
-            ))}
+                    </Box>
+                  </Stack>
+                </CardContent>
+              </Card>
+              {index !== data.length - 1 && <Divider />}
+            </Box>
+          ))
+        ) : (
+          <Typography variant="h5" color="error">
+            {data?.response?.data?.message}
+          </Typography>
+        )}
       </Stack>
     </Stack>
   );
