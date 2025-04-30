5eb3c27c097eab2da6ddd8bcf9ffcd8f46a76dd5
priya <priyankbhalala1911@gmail.com>
2025-04-30 13:27:58 +0530

    To Change city value on dynamically

--- src/Section/SlotBookingPage/CityLocation.tsx
+++ src/Section/SlotBookingPage/CityLocation.tsx
@@ -1,17 +1,25 @@
 import { PlaceOutlined } from "@mui/icons-material";
 import MenuItemSelect from "../../components/MenuItem";
 
-const cities = ["Ahmedabad", "Surat"];
+// const cities = ["Ahmedabad", "Surat"];
 
-const city = cities.map((city) => ({ label: city, value: city }));
+// const city = cities.map((city) => ({ label: city, value: city }));
 
-const cityObjects = [{ label: "", value: "Select City" }, ...city];
+// const cityObjects = [{ label: "", value: "Select City" }, ...city];
 
-const CityLocation: React.FC = () => {
+interface CityLocationProps {
+  city?: string[];
+}
+
+const CityLocation: React.FC<CityLocationProps> = ({ city = [] }) => {
+  const cityObject = [
+    { label: "", value: "Select City" },
+    ...city.map((city) => ({ label: city, value: city })),
+  ];
   return (
     <>
       <MenuItemSelect
-        record={cityObjects}
+        record={cityObject}
         startIcon={<PlaceOutlined />}
         fontSize="20px"
         searchField
